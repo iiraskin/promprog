9d9d1b035f8472eca7184c47bcd9acdfda7a7461
iiraskin <iraskin@mail.ru>
2019-03-05 11:43:40 +0300

    Update producer.py

--- producer/producer.py
+++ producer/producer.py
@@ -5,28 +5,25 @@ import time
 import random
 
 
-time.sleep(50)
-
-params = pika.ConnectionParameters('rabbitmq', 5672)
-connection = pika.BlockingConnection(params)
-channel = connection.channel()
-channel.queue_declare(queue='random_queue')
+time.sleep(30)
 
 while True:
-    num = random.randint(-1000, 1000)
-	
     try:
-        channel.basic_publish(exchange='', routing_key='random_queue', body=str(num))
-        print('Sent:', num)
-	
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
+        channel = connection.channel()
+
+        channel.queue_declare(queue = 'random_queue')
+        while True:
+            num = random.randint(-1000, 1000)
+            
+            channel.basic_publish(exchange='', routing_key='random_queue', body=str(num))
+            print('Sent:', num)
+            
+            time.sleep(random.randint(1, 10))
+            
     except pika.exceptions.ConnectionClosed:
-        print('Connection is closed.')
+        print('Connection is closed')
     except Exception as ex:
         print(ex)
 
-    sleep = random.randint(1, 10)
-    time.sleep(sleep)
-    
-print(2)
 connection.close()
-
